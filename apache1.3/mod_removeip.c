ad6e7663691b1d308034635dab74c6588cde3182
Alfred Toth <alfred.toth@orbiz.com>
2015-04-07 15:49:46 +0200

    Übernehme IP-Adresse aus X-Forwarded-For als RemoteIP

--- apache1.3/mod_removeip.c
+++ apache1.3/mod_removeip.c
@@ -32,6 +32,39 @@ static const char *removeip_enable(cmd_parms *cmd, void *dummy, int flag) {
     return NULL;
 }
 
+static int looks_like_ip(const char *ip) {
+    static const char ipv4_set[] = "0123456789./";
+    if (!*ip)
+        return 0;
+    const char *ptr = ip;
+    int c = 0;
+    while(*ptr && strchr(ipv4_set, *ptr) != NULL)
+        ++ptr;
+    return (*ptr == '\0');
+}
+
+static int lastValue(const char *s) {
+    if (!*s)
+        return -1;
+    const char *ptr = s;
+    while (*ptr) {
+        const char *s2 = strchr(ptr, ',');
+        if (s2) {
+            ptr = s2 + 1;
+        } else {
+          break;
+        }
+    }
+    while (*ptr) {
+        if (*ptr == ' ') {
+            ++ptr;
+        } else {
+            break;
+        }
+    }
+
+    return ptr - s;
+}
 
 static int change_remote_ip(request_rec *r) {
     const char *fwdvalue;
@@ -39,10 +72,30 @@ static int change_remote_ip(request_rec *r) {
     removeip_server_cfg *cfg = (removeip_server_cfg *)ap_get_module_config(r->server->module_config,
                                                                    &removeip_module);
 
-    if (!cfg->enable)
-        return DECLINED;
+/*    if (!cfg->enable)
+        return DECLINED;*/
+
+    fwdvalue = ap_table_get(r->headers_in, "X-Forwarded-For");
 
     uint32_t mask = ~((1U << 8) - 1);
+    if (fwdvalue) {
+        char ip[] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+        int k = lastValue(fwdvalue);
+        if (k > 0) {
+            int l = strlen(fwdvalue + k);
+            if (l <= 15) {
+                strncpy(ip, fwdvalue + k, l);
+            }
+        } else {
+            int l = strlen(fwdvalue + k);
+            if (l <= 15) {
+                strncpy(ip, fwdvalue, l);
+            }
+        }
+        if (looks_like_ip(ip) != 0) {
+            inet_pton(AF_INET, ip, &(r->connection->remote_addr.sin_addr));
+        }
+    }
     r->connection->remote_addr.sin_addr.s_addr &= htonl(mask);
     r->connection->remote_ip = ap_pstrdup(r->connection->pool, inet_ntoa(r->connection->remote_addr.sin_addr));
 
